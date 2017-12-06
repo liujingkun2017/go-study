c6f2c8bb7d562e1bbb58b7857675a37bfc9fca43
liujingkun <13164432553@163.com>
2017-12-06 17:17:52 +0800

    buffer demo

---
+++ src/demo5/demo5.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func testbuffer() {
+
+	c := make(chan int, 3);
+	for i := 1; i < 5; i++ {
+		go w2c(c)
+	}
+
+	for i:=range  c{
+		fmt.Println("receivced value ",i)
+	}
+
+}
+
+func w2c(c chan int) {
+	for {
+		select {
+		case c <- 0:
+			fmt.Println("w2c value 0")
+		case c <- 1:
+			fmt.Println("w2c value 1")
+		case c <- 2:
+			fmt.Println("w2c value 2")
+		case c <- 3:
+			fmt.Println("w2c value 3")
+		}
+	}
+
+}
+
+
+func main() {
+	testbuffer()
+}
